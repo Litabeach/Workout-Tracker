b8835112ffea29e274dd17d411c79709306ab8ae
Lita Beach <litabeach123@gmail.com>
2021-04-09 10:40:28 -0500

    db seeded

--- models/Workout.js
+++ models/Workout.js
@@ -1,16 +1,35 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    type: String,
-    name: String,
-    duration: Number,
-    weight: Number,
-    reps: Number,
-    sets: Number,
+
+    day: {
+        type: Date,
+        default: Date.now
+    },
+
+
+    excersizes: [],
+
+    // type: String,
+    // name: String,
+    // duration: Number,
+    // weight: Number,
+    // reps: Number,
+    // sets: Number,
+
 });
+
+// db.workout.aggregate([
+//     {
+//         $addFields: {
+//             totalWeight: { $sum: "$weight" },
+//             totalDuration: { $sum: "$duration" }
+//         }
+//     },
+// ]);
+
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
-module.exports = Book;
+module.exports = Workout;
